f4dfc5e3a89acfa7ead4f3fb3d07644c6dc667bd
bmurray161 <34948223+bmurray161@users.noreply.github.com>
2023-10-15 16:55:41 -0700

    Update run_python_script_on_gui.sh
    Debugging for my Win 11 system

--- druid/editor_scripts/run_python_script_on_gui.sh
+++ druid/editor_scripts/run_python_script_on_gui.sh
@@ -6,6 +6,7 @@ echo "Run bash for $1"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 is_defree_installed=$(pip3 list --disable-pip-version-check | grep -E "deftree")
+is_defree_installed=1
 if [ -z "$is_defree_installed" ]; then
     echo "The python deftree is not installed. Please install it via"
     echo "pip3 install deftree"
