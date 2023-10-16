16823adcbc64eab5c297199d2b6cfa957165bc2d
bmurray161 <34948223+bmurray161@users.noreply.github.com>
2023-10-15 18:08:54 -0700

    Update run_python_script_on_gui.sh

--- druid/editor_scripts/run_python_script_on_gui.sh
+++ druid/editor_scripts/run_python_script_on_gui.sh
@@ -5,7 +5,7 @@
 echo "Run bash for $1"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-is_defree_installed=$(pip3 list --disable-pip-version-check | grep -E "deftree")
+is_defree_installed=$(pip3b list --disable-pip-version-check | grep -E "deftree")
 is_defree_installed=1
 if [ -z "$is_defree_installed" ]; then
     echo "The python deftree is not installed. Please install it via"
@@ -13,5 +13,5 @@ if [ -z "$is_defree_installed" ]; then
     exit 0
 fi
 
-C:\Users\Brian Murray\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\python3 $1 $2
+python3b $1 $2
 
